33aa7be56b612cc99ca81841cd4255d50f5b82cf
Rich Comber <rich@elementalwebdesign.co.uk>
2017-02-25 15:56:07 +0000

    Setting up migrations and model details for the products categories

--- app/ProductCategory.php
+++ app/ProductCategory.php
@@ -1,6 +1,8 @@
 <?php
 namespace App;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
 use Franzose\ClosureTable\Models\Entity;
 
 class ProductCategory extends Entity implements ProductCategoryInterface
@@ -10,7 +12,9 @@ class ProductCategory extends Entity implements ProductCategoryInterface
      *
      * @var string
      */
+
     protected $table = 'product_categories';
+    protected $dateFormat = 'U';
 
     /**
      * ClosureTable model instance.
@@ -18,4 +22,32 @@ class ProductCategory extends Entity implements ProductCategoryInterface
      * @var ProductCategoryClosure
      */
     protected $closure = 'App\ProductCategoryClosure';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'slug', 'online', 'seo_title', 'seo_description'
+    ];
+
+    protected $casts = [
+        'online' => 'boolean'
+    ];
+
+    public function scopeActive($query)
+    {
+        return $query->where('online', '=', true);
+    }
+
+    public function scopeInactive($query)
+    {
+        return $query->where('online', '=', false);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Product::class, App::make(ProductCategory::class)->getKeyName());
+    }
 }
